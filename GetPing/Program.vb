fcf8f20627286a11597089f633350eab60fcc081
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-09-01 03:05:16 +0330

    - Update and Edit Banner Function

--- GetPing/Program.vb
+++ GetPing/Program.vb
@@ -7,12 +7,12 @@
 
     Sub Banner()
         Console.ForegroundColor = ConsoleColor.Green
-        Console.WriteLine("    ____  _                ______          __              ________ ")
-        Console.WriteLine("   / __ \(_)___  ____ _   /_  __/__  _____/ /____  __  _  |__  /__ \")
-        Console.WriteLine("  / /_/ / / __ \/ __ `/    / / / _ \/ ___/ __/ _ \/  /_/   /_ <__/ /")
-        Console.WriteLine(" / ____/ / / / / /_/ /    / / /  __(__  ) /_/  __/  /    ___/ / __/ ")
-        Console.WriteLine("/_/   /_/_/ /_/\__, /    /_/  \___/____/\__/\___/\_/    /____/____/ ")
-        Console.WriteLine("              /____/                                                 ")
+        Console.WriteLine("    ____  _                ______          __           ")
+        Console.WriteLine("   / __ \(_)___  ____ _   /_  __/__  _____/ /____  __  _")
+        Console.WriteLine("  / /_/ / / __ \/ __ `/    / / / _ \/ ___/ __/ _ \/  /_/")
+        Console.WriteLine(" / ____/ / / / / /_/ /    / / /  __(__  ) /_/  __/  /   ")
+        Console.WriteLine("/_/   /_/_/ /_/\__, /    /_/  \___/____/\__/\___/\_/    ")
+        Console.WriteLine("              /____/                                    ")
 
     End Sub
 
