a179ea802c73ac0b9d9af7abbfde3ee10308d57f
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-31 21:52:26 +0330

    - Create Banner Function

--- GetPing/Program.vb
+++ GetPing/Program.vb
@@ -4,4 +4,14 @@
 
     End Sub
 
+    Sub Banner()
+        Console.ForegroundColor = ConsoleColor.Green
+        Console.WriteLine("    ____  _                ______          __              ________ ")
+        Console.WriteLine("   / __ \(_)___  ____ _   /_  __/__  _____/ /____  __  _  |__  /__ \")
+        Console.WriteLine("  / /_/ / / __ \/ __ `/    / / / _ \/ ___/ __/ _ \/  /_/   /_ <__/ /")
+        Console.WriteLine(" / ____/ / / / / /_/ /    / / /  __(__  ) /_/  __/  /    ___/ / __/ ")
+        Console.WriteLine("/_/   /_/_/ /_/\__, /    /_/  \___/____/\__/\___/\_/    /____/____/ ")
+        Console.WriteLine("              /____/                                                 ")
+    End Sub
+
 End Module
