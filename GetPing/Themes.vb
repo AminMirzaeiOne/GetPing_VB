006e6899aa0c0a87c6e7f6b087ca4d3f3c4a4bad
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-09-01 06:41:03 +0330

    - Update User Interface and Create QuestionColor Function

--- GetPing/Themes.vb
+++ GetPing/Themes.vb
@@ -21,4 +21,34 @@ question:
             Return False
         End If
     End Function
+
+    Public Sub QuestionColor()
+questioncolor:
+        System.Console.Write("Enter Your Theme Color (Black - White - Red - Blue - Green - Magenta - Yellow) : ")
+        Dim color As String = System.Console.ReadLine().ToLower()
+        Select Case color
+            Case "black"
+                System.Console.ForegroundColor = ConsoleColor.Black
+            Case "black"
+                System.Console.ForegroundColor = ConsoleColor.Black
+            Case "red"
+                System.Console.ForegroundColor = ConsoleColor.Black
+            Case "blue"
+                System.Console.ForegroundColor = ConsoleColor.Black
+            Case "green"
+                System.Console.ForegroundColor = ConsoleColor.Black
+            Case "magenta"
+                System.Console.ForegroundColor = ConsoleColor.Black
+            Case "yellow"
+                System.Console.ForegroundColor = ConsoleColor.Black
+            Case Else
+                System.Console.BackgroundColor = ConsoleColor.Red
+                System.Console.ForegroundColor = ConsoleColor.White
+                System.Console.WriteLine("Theme Color It is invalid")
+                System.Console.ResetColor()
+                Console.ReadKey()
+                GoTo questioncolor
+
+        End Select
+    End Sub
 End Module
